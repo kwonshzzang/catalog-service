8bce4d850b48ef31380684505598dc2fa581dabc
kwonshzzang <kwonshzzang@gmail.com>
2024-06-11 14:17:08 +0900

    First Commit

---
+++ src/test/java/kr/co/kwonshzzang/catalogservice/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kwonshzzang.catalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
