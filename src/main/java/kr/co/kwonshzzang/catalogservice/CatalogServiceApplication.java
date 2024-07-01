bc2b7f0b5644c87aa173506ace8761c055a50d90
kwonshzzang <kwonshzzang@gmail.com>
2024-07-01 18:06:17 +0900

    Add spring properties

--- src/main/java/kr/co/kwonshzzang/catalogservice/CatalogServiceApplication.java
+++ src/main/java/kr/co/kwonshzzang/catalogservice/CatalogServiceApplication.java
@@ -2,8 +2,10 @@ package kr.co.kwonshzzang.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
