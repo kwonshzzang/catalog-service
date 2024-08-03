ccbc931b7aa8430ac07295179ee37080c2893cf4
kwonshzzang <kwonshzzang@gmail.com>
2024-08-03 12:59:42 +0900

    restore repository

---
+++ src/main/java/kr/co/kwonshzzang/catalogservice/HomeController.java
@@ -0,0 +1,18 @@
+package kr.co.kwonshzzang.catalogservice;
+
+import kr.co.kwonshzzang.catalogservice.config.PolarProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class HomeController {
+    private final PolarProperties polarProperties;
+
+    @GetMapping("/")
+    public String getGreeting() {
+//        return "도서 카탈로그에 오신 것을 환영합니다!";
+        return polarProperties.getGreeting();
+    }
+}
---
+++ src/main/java/kr/co/kwonshzzang/catalogservice/domain/BookRepository.java
@@ -0,0 +1,18 @@
+package kr.co.kwonshzzang.catalogservice.domain;
+
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+    Optional<Book> findByIsbn(String isbn);
+    boolean existsByIsbn(String isbn);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Book WHERE ISBN = :isbn")
+    void deleteByIsbn(String isbn);
+}
